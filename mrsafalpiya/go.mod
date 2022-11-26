f42a46e8057cc253e17fb7f43f852ceefbe25596
Safal Piya <mrsafalpiya@gmail.com>
2022-11-26 22:01:27 +0545

    Implemented (very ugly) thumbnail link parser.

--- mrsafalpiya/go.mod
+++ mrsafalpiya/go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
+	github.com/aymerick/douceur v0.2.0
 	github.com/gocolly/colly v1.2.0
 )
 
@@ -16,6 +17,7 @@ require (
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
--- mrsafalpiya/main.go
+++ mrsafalpiya/main.go
@@ -4,8 +4,11 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/asaskevich/govalidator"
+	"github.com/aymerick/douceur/css"
+	"github.com/aymerick/douceur/parser"
 	"github.com/gocolly/colly"
 )
 
@@ -36,21 +39,75 @@ func getBlogs(link string, divClass string, bgDivClass string) ([]blog, error) {
 		return blogs, errors.New("Given link is not an URL")
 	}
 
+	var styleBody string
+	var stylesheets []*css.Stylesheet
+
 	c := colly.NewCollector()
+
+	c.OnHTML("style[data-emotion]", func(e *colly.HTMLElement) {
+		styleBody = e.Text
+
+		stylesheet, err := parser.Parse(styleBody)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		stylesheets = append(stylesheets, stylesheet)
+	})
+
 	c.OnHTML(divClass, func(e *colly.HTMLElement) {
 		title := e.ChildText("h2")
 
+		childDivs := e.ChildAttrs("div", "class")
+		bgDiv := "." + strings.Split(childDivs[len(childDivs)-1], " ")[0]
+		_ = bgDiv
+
+		var bgUrl string
+		for _, style := range stylesheets {
+			for _, rule := range style.Rules {
+				if len(rule.Selectors) > 0 && rule.Selectors[0] == bgDiv {
+					for _, decl := range rule.Declarations {
+						if decl.Property == "background-image" {
+							bgUrl = urlFromCSSVal(decl.Value)
+						}
+					}
+				}
+			}
+		}
+
 		blogs = append(blogs, blog{
 			title:         title,
-			thumbnailLink: "",
+			thumbnailLink: bgUrl,
 		})
 	})
+
 	c.Visit(link)
 
 	return blogs, nil
 }
 
-/* File Handling */
+/* Helper */
+
+/*
+ * Extracts the URL parameter from the CSS value.
+ *
+ * Example: `url(//foo)` -> `foo`
+ */
+func urlFromCSSVal(s string) string {
+	i := strings.Index(s, "(")
+	if i >= 0 {
+		j := strings.Index(s, ")")
+		if j >= 0 {
+			k := 0
+			for ; k < len(s)-i; k++ {
+				if s[i+k+1] != '/' {
+					break
+				}
+			}
+			return s[i+k+1 : j] /* the link has leading `//` */
+		}
+	}
+	return ""
+}
 
 /* Entry point */
 
@@ -61,6 +118,6 @@ func main() {
 	}
 
 	for _, blog := range blogs {
-		fmt.Println(blog.title)
+		fmt.Println(blog.title, blog.thumbnailLink)
 	}
 }
