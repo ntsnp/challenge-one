51decdff8263b01ed0e1c01186b32142b2c07185
Rabin <98199146+initialrise@users.noreply.github.com>
2022-12-04 11:14:58 +0545

    Added cron job installer

---
+++ initialrise/cron.sh
@@ -0,0 +1,6 @@
+#$CWD = `pwd`
+#$CRONJOB = "0 12 * * * /usr/bin/python3 `pwd`/main.py"
+ 
+(crontab -l ; echo "0 12 * * * /usr/bin/python3 `pwd`/main.py -d `date +20%y%m%d-%H%M%S`.db") | crontab -
+
+
